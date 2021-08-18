a6010af258072c8ee0d21e8dd8727fc1d2e690dc
Ankur-Deka <ankur.deka@iitg.ac.in>
2021-08-17 22:35:18 -0400

    reach env wrapper added to mujocofh

--- envs/mujocoFH.py
+++ envs/mujocoFH.py
@@ -1,13 +1,15 @@
 # Fixed Horizon wrapper of mujoco environments
 import gym, gym_vecenv
+from gym import spaces
 import numpy as np
+
 
 import sys, os
 sys.path.append(os.path.join(sys.path[0],'../mape'))
 from multiagent.environment import MultiAgentEnv
 import multiagent.scenarios as scenarios
 import time
-
+from icecream import ic
 def make_multiagent_env(env_id, num_agents, dist_threshold, arena_size, identity_size, num_steps=128, diff_reward=True, same_color = False, random_leader_name=False, goal_at_top=False, hide_goal=False, video_format='webm'):
     scenario = scenarios.load(env_id+".py").Scenario(num_agents=num_agents, dist_threshold=dist_threshold,
                                                      arena_size=arena_size, identity_size=identity_size, num_steps=num_steps, diff_reward=diff_reward, same_color = same_color, random_leader_name=random_leader_name, goal_at_top=goal_at_top, hide_goal=hide_goal)
@@ -79,3 +81,56 @@ class MujocoFH(gym.Env):
 
     def render(self):
         self.env.render()
+
+
+class FetchFH(MujocoFH):
+    def __init__(self,*args,**kwargs):
+        super(FetchFH,self).__init__(*args,**kwargs)
+        self.observation_space = spaces.Box(-np.inf, np.inf, shape=[2], dtype='float32')  
+        self.action_space = spaces.Box(-1., 1., shape=(2,), dtype='float32')
+        self.reset()
+
+    # use fixed goal
+    def extract_relative_goal_pos(self, obs):    
+        grip_pos = obs['observation'][:2] # x.y,z position
+        goal_pos_rel = self.goal - grip_pos
+        return goal_pos_rel
+
+    def reset(self):
+        self.t = 0
+        self.terminated = False
+        self.terminal_state = None
+        obs = self.env.reset()
+        # self.obs = self.normalize_obs(self.obs)
+        goal_pos_rel = np.array([0.15,0])
+        goal_pos_rel_3d = np.concatenate((goal_pos_rel, [0]))
+        # ic(self.env.goal)
+        self.env.env.goal = self.env.initial_gripper_xpos[:3] + goal_pos_rel_3d
+        self.goal = self.env.env.goal[:2]
+        self.obs = goal_pos_rel
+        return self.obs.copy()
+
+    def step(self, action):
+        self.t += 1
+
+        if self.terminated:
+            return self.terminal_state, 0, self.t == self.T, True
+        else:
+            prev_obs = self.obs.copy()
+            action = np.concatenate((action,np.zeros(2)))
+            obs, r, done, info = self.env.step(action)
+            self.obs = self.extract_relative_goal_pos(obs)
+            # self.obs = self.normalize_obs(self.obs)
+            
+            if self.r is not None:  # from irl model
+                r = self.r(prev_obs)
+
+            if done:
+                self.terminated = True
+                self.terminal_state = self.obs
+            
+            return self.obs.copy(), r, done, done
+        # print(self.obs)
+        return self.obs.copy(),reward,done,info    
+
+
--- test/test_fetch.py
+++ test/test_fetch.py
@@ -1,25 +1,30 @@
 import gym, time
+import envs
 from icecream import ic
 import numpy as np
 
-env = gym.make('PlanarReachGoal1Dense-v1')
-
+env = gym.make('PlanarReachGoal1DenseFH-v0')
+print(env.__dict__)
 obs = env.reset()
 
-def p_controller(obs, gain=10):
-    action = np.concatenate((gain*obs,np.zeros(1)))
+def p_controller(obs, gain=20, max_val=1):
+    action = np.clip(gain*obs, -max_val, max_val)
     return action
 
+r_total = 0
 for episode in range(10):
     ic(episode)
     obs = env.reset()
     done = False
     for t in range(20):
-        # a = env.action_space.sample()
-        ic(obs)
+        a = env.action_space.sample()
+
         a = p_controller(obs)
+        ic(a.round(2),obs.round(2))
         obs,rew,done,info = env.step(a)
+        r_total += rew
         # print(np.round(obs,2))
         print(rew)
         env.render()
-        time.sleep(0.1)
+        # time.sleep(0.1)
+print(r_total/10)
