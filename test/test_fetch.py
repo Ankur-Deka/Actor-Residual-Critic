7e437c37a8f195b17cb5f23ef315e6e2f1490377
Ankur-Deka <ankur.deka@iitg.ac.in>
2021-08-17 20:27:48 -0400

    fetch environment test code

--- test/test_fetch.py
+++ test/test_fetch.py
@@ -1,20 +1,25 @@
-import gym
+import gym, time
 from icecream import ic
+import numpy as np
 
-env = gym.make('FetchReach-v1')
-ic(env.state_space(), env.action_space())
+env = gym.make('PlanarReachGoal1Dense-v1')
 
+obs = env.reset()
 
+def p_controller(obs, gain=10):
+    action = np.concatenate((gain*obs,np.zeros(1)))
+    return action
 
-# ---------- getting env name ---------- #
-# env_dict = gym.envs.registry.all()
-# for k in env_dict:
-# 	print(k)
-# EnvSpec(FetchSlide-v1)
-# EnvSpec(FetchPickAndPlace-v1)
-# EnvSpec(FetchReach-v1)
-# EnvSpec(FetchPush-v1)
-# EnvSpec(FetchSlideDense-v1)
-# EnvSpec(FetchPickAndPlaceDense-v1)
-# EnvSpec(FetchReachDense-v1)
-# EnvSpec(FetchPushDense-v1)
+for episode in range(10):
+    ic(episode)
+    obs = env.reset()
+    done = False
+    for t in range(20):
+        # a = env.action_space.sample()
+        ic(obs)
+        a = p_controller(obs)
+        obs,rew,done,info = env.step(a)
+        # print(np.round(obs,2))
+        print(rew)
+        env.render()
+        time.sleep(0.1)
